597ccb3e75d8f758cf88e82e0d58bbb4bccab4d0
DFoxArdent <DarylFox@ardent-management.com>
2024-02-06 10:30:09 +0000

    Stability updates

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -710,7 +710,7 @@ const Chat = () => {
                                         },
                                         root: {
                                             color: '#FFFFFF',
-                                            background: "radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)"
+                                            background: "radial-gradient(circle, #87F5D3 1%, #00DA96 60%)"
                                         },
                                         rootDisabled: {
                                             background: "#F0F0F0"
@@ -733,7 +733,7 @@ const Chat = () => {
                                         },
                                         root: {
                                             color: '#FFFFFF',
-                                            background: "radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)",
+                                            background: "radial-gradient(circle, #87F5D3 1%, #00DA96 60%)",
                                         },
                                         rootDisabled: {
                                             background: "#F0F0F0"
